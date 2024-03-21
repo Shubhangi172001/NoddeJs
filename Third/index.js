1d3551f4b757471283a111a162f1c742a90e01fa
Shubhangi172001 <shubhangikompelli7060@gmail.com>
2024-03-21 12:58:32 +0530

    initial

--- Third/index.js
+++ Third/index.js
@@ -1,7 +1,21 @@
 const http=require("http")
+const url=require("url")
+const fs=require("fs")
+
 const myServer=http.createServer((req,res)=>{
-    console.log("New Request Arrived")
-    res.end("Hello from Server")
+    //res.end("Hello From Server")
+    const myUrl=url.parse(req.url)
+    console.log(myUrl)
+    switch(myUrl.pathname){
+        case '/':res.end("Home Page");break;
+        case '/contact':res.end("Contact no:9087654321\nEmail id:abd@gmail.com");break;
+        case '/about':res.end("I am Student of Btech ELN");break;
+        default:res.end("404 Page not Found")
+    }
+    // console.log("Protocol=",myUrl.protocol)
+    // console.log("domain=",myUrl.domain)
+    // console.log("Pathname=",myUrl.Pathname)
+    // console.log("Query=",myUrl.Query)
 })
 myServer.listen(8000,()=>{
     console.log("Server Created")
